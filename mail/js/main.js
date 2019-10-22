bfc1581c7a5fc4403a35e672451073be34637a1a
MATVEN <mattiavena2993@gmail.com>
2019-10-22 16:16:23 +0200

    ex mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,19 +1,16 @@
 // variabili
-var ask = parseInt(prompt("Scrivi qui la tua mail."));
-var list = ['member1@mail.com', 'member2@mail.com', 'member3@mail.com', 'member4@mail.com'];
-console.log(list);
-
-var mail = list;
+var mailUser = prompt("Scrivi qui la tua mail.");
+console.log(mailUser);
+var listMail = ['member1@mail.com', 'member2@mail.com', 'member3@mail.com', 'member4@mail.com'];
+console.log(listMail);
 var found = false;
-console.log(found, 'before for')
+
 // verifica
-for (var i = 0; i < mail.length; i++) {
-  if (mail == list[i]) {
+for (var i = 0; i < listMail.length; i++) {
+  if (mailUser == listMail[i]) {
     found = true;
   }
 }
-
-console.log(found, 'after for')
 
 if (found == true) {
   console.log('Congratulations!You are on the invited list.');
